a2374065b69c619853722baec02688c2da6c8e6a
Pyae Soan Aung <server01.psa1981@gmail.com>
2025-07-26 04:10:42 +0630

    rename

---
+++ src/rom_dynamics_special_mode/src/rom_follow_road.cpp
@@ -0,0 +1,17 @@
+#include "rclcpp/rclcpp.hpp"
+
+#include <rom_dynamics_special_mode/rom_follow_road.hpp>
+#include <px4_ros2/components/node_with_mode.hpp>
+
+using MyNodeWithMode = px4_ros2::NodeWithMode<FlightModeTest>;
+
+static const std::string kNodeName = "rom_follow_road";
+static const bool kEnableDebugOutput = true;
+
+int main(int argc, char * argv[])
+{
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<RoadFollowMode>(kNodeName, kEnableDebugOutput));
+  rclcpp::shutdown();
+  return 0;
+}
